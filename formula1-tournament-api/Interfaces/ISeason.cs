be892e4393edc7d4c14654c23ef3fe42e2bff319
leventenyiro <nyiro.levente@gmail.com>
2022-07-24 21:51:32 +0200

    Add season interface

---
+++ formula1-tournament-api/Interfaces/ISeason.cs
@@ -0,0 +1,11 @@
+﻿namespace formula1_tournament_api.Interfaces
+{
+    public interface ISeason
+    {
+        Task<(bool IsSuccess, List<SeasonDTO> Season, string ErrorMessage)> GetAllSeasons();
+        Task<(bool IsSuccess, SeasonDTO Season, string ErrorMessage)> GetSeasonById(Guid id);
+        Task<(bool IsSuccess, string ErrorMessage)> AddSeason(SeasonDTO season);
+        Task<(bool IsSuccess, string ErrorMessage)> UpdateSeason(Guid id, SeasonDTO season);
+        Task<(bool IsSuccess, string ErrorMessage)> DeleteSeason(Guid id);
+    }
+}
