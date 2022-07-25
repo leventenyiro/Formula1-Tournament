9ffcde4d3a4968acc2661a696967f02873fe9ae7
leventenyiro <nyiro.levente@gmail.com>
2022-07-25 22:00:07 +0200

    Add dependency of season

--- formula1-tournament-api/Program.cs
+++ formula1-tournament-api/Program.cs
@@ -1,4 +1,6 @@
 using formula1_tournament_api.Data;
+using formula1_tournament_api.Interfaces;
+using formula1_tournament_api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,8 @@ builder.Services.AddDbContext<FormulaDbContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ISeason, SeasonService>();
 
 var app = builder.Build();
 
