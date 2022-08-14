44271863e4ff77cae82295a2a582bd1fefce7ee1
leventenyiro <nyiro.levente@gmail.com>
2022-08-14 20:33:32 +0200

    add header and login, but not ready

--- formula1-tournament-web/src/app/login/login.component.ts
+++ formula1-tournament-web/src/app/login/login.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onLogin(data: any) {
+    this.isFetching = true
+    /*if (condition) {
+      data.form.controls["name"].status = "INVALID"
+      this.errorName = "Name must be 3 char"
+    } else {*/
+      this.userService.addUser(data.value).subscribe({
+        next: () => this.router.navigate(['user']),
+        error: err => this.error = err,
+        complete: () => this.isFetching = false
+      })
+    //}
+  }
 }
