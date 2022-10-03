d6086e6d33ae931d05c3aa82e834bde2f8cd9d92
leventenyiro <nyiro.levente@gmail.com>
2022-10-03 18:51:02 +0200

    add RealName and Number to driver and modify TeamId to ActualTeamId

--- formula1-tournament-api/Models/Driver.cs
+++ formula1-tournament-api/Models/Driver.cs
@@ -4,10 +4,12 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public Team Team { get; set; }
+        public string RealName { get; set; }
+        public int Number { get; set; }
+        public Team ActualTeam { get; set; }
         public Season Season { get; set; }
 
-        public Guid TeamId { get; set; }
+        public Guid ActualTeamId { get; set; }
         public Guid SeasonId { get; set; }
         public List<Race> Races { get; set; }
     }
