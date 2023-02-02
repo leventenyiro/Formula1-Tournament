95a422e8e95df19817a17a34486593af360c4ebb
leventenyiro <nyiro.levente@gmail.com>
2023-02-02 10:15:30 +0100

    refactor the UserTests.cs

--- car-racing-tournament-api/Tests/Unit/UserTests.cs
+++ car-racing-tournament-api/Tests/Unit/UserTests.cs
@@ -10,32 +10,33 @@ namespace car_racing_tournament_api.Tests.Unit
     [TestFixture]
     public class UserTests
     {
+        private CarRacingTournamentDbContext? _context;
+        private UserService? _userService;
+
         [SetUp]
         public void Init()
-        {
-
-        }
-
-        [Test]
-        public async Task Login()
         {
             var options = new DbContextOptionsBuilder<CarRacingTournamentDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
 
-            using (var context = new CarRacingTournamentDbContext(options))
-            {
-                context.Users.Add(new User { Username = "username", Email = "test@test.com", Password = "$2a$10$/Mw2QNUGYbV1AIyQ8QxXC.IhNRrmjwAW9SBgUv8Vh9xX2goWsQwG." });
-                context.SaveChanges();
+            _context = new CarRacingTournamentDbContext(options);
 
-                var configuration = new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json")
-                    .Build();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-                var userService = new UserService(context, configuration);
-                var result = await userService.Login(new LoginDto { UsernameEmail = "username", Password = "Password1" });
-                Assert.IsTrue(result.IsSuccess);
-            }
+            _userService = new UserService(_context, configuration);
+        }
+
+        [Test]
+        public async Task Login()
+        {
+            _context?.Users.Add(new User { Username = "username", Email = "test@test.com", Password = "$2a$10$/Mw2QNUGYbV1AIyQ8QxXC.IhNRrmjwAW9SBgUv8Vh9xX2goWsQwG." });
+            _context?.SaveChanges();
+
+            var result = await _userService!.Login(new LoginDto { UsernameEmail = "username", Password = "Password1" });
+            Assert.IsTrue(result.IsSuccess);
         }
     }
 }
