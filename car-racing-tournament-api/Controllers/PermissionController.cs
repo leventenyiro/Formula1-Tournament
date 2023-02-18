b612bc60fa09ed72f666f1f8577ce6ba0bbad074
leventenyiro <nyiro.levente@gmail.com>
2023-02-18 17:42:06 +0100

    96: modify permission ready

--- car-racing-tournament-api/Controllers/PermissionController.cs
+++ car-racing-tournament-api/Controllers/PermissionController.cs
@@ -1,4 +1,6 @@
-﻿using car_racing_tournament_api.Interfaces;
+﻿using car_racing_tournament_api.DTO;
+using car_racing_tournament_api.Interfaces;
+using car_racing_tournament_api.Models;
 using car_racing_tournament_api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +18,25 @@ namespace car_racing_tournament_api.Controllers
             _permissionService = permissionService;
         }
 
+        [HttpPut("{id}"), Authorize]
+        public async Task<IActionResult> Put(Guid id, [FromForm] PermissionType permissionType)
+        {
+            var resultGetPermission = await _permissionService.GetPermissionById(id);
+            if (!resultGetPermission.IsSuccess)
+                return NotFound(resultGetPermission.ErrorMessage);
+
+            if (!await _permissionService.IsAdmin(new Guid(User.Identity!.Name!), resultGetPermission.Permission!.SeasonId))
+                return Forbid();
+
+            // if downgrade himself, oldest moderator
+
+            var resultUpdate = await _permissionService.UpdatePermissionType(resultGetPermission.Permission, permissionType);
+            if (!resultUpdate.IsSuccess)
+                return BadRequest(resultUpdate.ErrorMessage);
+
+            return NoContent();
+        }
+
         [HttpDelete("id"), Authorize]
         public async Task<IActionResult> Delete(Guid id)
         {
@@ -23,8 +44,10 @@ namespace car_racing_tournament_api.Controllers
             if (!resultGet.IsSuccess)
                 return NotFound(resultGet.ErrorMessage);
 
-            if (!await _permissionService.IsAdminModerator(new Guid(User.Identity!.Name!), resultGet.Permission!.SeasonId))
+            if (!await _permissionService.IsAdmin(new Guid(User.Identity!.Name!), resultGet.Permission!.SeasonId))
                 return Forbid();
+
+            // if delete himself, oldest moderator
 
             var resultDelete = await _permissionService.RemovePermission(resultGet.Permission);
             if (!resultDelete.IsSuccess)
