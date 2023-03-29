52bd73f9518934b770b840a8ac528d25f9888f83
leventenyiro <nyiro.levente@gmail.com>
2023-03-29 23:01:54 +0200

    150: add location.back

--- car-racing-tournament-web/src/app/pages/login/login.component.ts
+++ car-racing-tournament-web/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   isFetching = false;
   error = "";
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private location: Location) { }
 
   ngOnInit(): void {
     if (this.authService.isSessionValid(document.cookie)) {
@@ -26,7 +27,7 @@ export class LoginComponent implements OnInit {
       next: (data) => {
         document.cookie = `session=${data}`;
         this.authService.loggedIn.emit(true);
-        this.router.navigate(['']);
+        this.location.back();
       },
       error: err => this.error = err,
       complete: () => this.isFetching = false
