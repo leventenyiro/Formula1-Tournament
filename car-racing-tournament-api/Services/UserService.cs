4dd3a13b4f2cad42da3d8ba89bf46f1f3618718d
Levente Nyírő <nyiro.levente@gmail.com>
2023-04-07 01:18:42 +0200

    Passwordupdate (#168)
    * add passwordOld
    
    * #115: solve update password

--- car-racing-tournament-api/Services/UserService.cs
+++ car-racing-tournament-api/Services/UserService.cs
@@ -21,7 +21,7 @@ namespace car_racing_tournament_api.Services
             _carRacingTournamentDbContext = carRacingTournamentDbContext;
             _configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
-                .Build();;
+                .Build();
         }
 
         public (bool IsSuccess, string? Token, string? ErrorMessage) Login(User user, string password, bool needToken)
@@ -76,7 +76,8 @@ namespace car_racing_tournament_api.Services
             .Select(x => new User {
                 Id = x.Id,
                 Username = x.Username,
-                Email = x.Email
+                Email = x.Email,
+                Password = x.Password
             }).FirstOrDefaultAsync();
             if (result == null)
                 return (false, null, _configuration["ErrorMessages:UserNotFound"]);
@@ -121,6 +122,9 @@ namespace car_racing_tournament_api.Services
 
         public async Task<(bool IsSuccess, string? ErrorMessage)> UpdatePassword(User user, UpdatePasswordDto updatePasswordDto)
         {
+            if (!BCrypt.Net.BCrypt.Verify(updatePasswordDto.PasswordOld, user.Password))
+                return (false, _configuration["ErrorMessages:LoginDetails"]);
+
             if (updatePasswordDto.Password != updatePasswordDto.PasswordAgain)
                 return (false, _configuration["ErrorMessages:PasswordsPass"]);
 
