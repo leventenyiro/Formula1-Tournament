7c0289f279382cb4b4f2e659b18e4d4b8706941a
Levente Nyírő <nyiro.levente@gmail.com>
2023-04-10 21:43:08 +0200

    update database (#179)
    * add migration init
    
    * main.yml debug
    
    * modify Program.cs
    
    * Modify Program.cs
    
    * fix yaml
    
    * fix yaml v2
    
    * add force in db update
    
    * fix yml
    
    * fix yml
    
    * add force
    
    * fix yml
    
    * ready with main.yml
    
    * ready with yml file

--- car-racing-tournament-api/Program.cs
+++ car-racing-tournament-api/Program.cs
@@ -22,12 +22,11 @@ internal class Program
         builder.Services.AddControllers();
         builder.Services.AddDbContext<CarRacingTournamentDbContext>(options =>
         {
-            if (builder.Environment.IsProduction())
-                options.UseSqlServer(builder.Configuration["ConnectionString"]);
-            else
-                options.UseMySql("Server=localhost;Uid=root;Database=car_racing_tournament;Pwd=;", 
-                    new MySqlServerVersion(new Version(8, 0, 22)), 
-                    options => options.EnableRetryOnFailure());
+            // options.UseMySql("Server=localhost;Uid=root;Database=car_racing_tournament;Pwd=;", 
+            // new MySqlServerVersion(new Version(8, 0, 22)), 
+            // options => options.EnableRetryOnFailure());
+            
+            options.UseSqlServer(builder.Configuration["ConnectionString"]);
             options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         });
 
