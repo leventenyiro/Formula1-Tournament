8b26830313f63ea0eafd13e086dafea9ab807b2e
leventenyiro <nyiro.levente@gmail.com>
2023-05-09 09:45:50 +0200

    #109: start implementing of PermissionTests

---
+++ car-racing-tournament-api/Tests/Integration/PermissionTests.cs
@@ -0,0 +1,193 @@
+using AutoMapper;
+using car_racing_tournament_api.Controllers;
+using car_racing_tournament_api.Data;
+using car_racing_tournament_api.DTO;
+using car_racing_tournament_api.Models;
+using car_racing_tournament_api.Profiles;
+using car_racing_tournament_api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System.Security.Claims;
+
+namespace car_racing_tournament_api.Tests.Integration
+{
+    [TestFixture]
+    public class PermissionTests
+    {
+        private PermissionController? _permissionController;
+        private CarRacingTournamentDbContext? _context;
+        private Permission? _permission;
+        private SeasonService? _seasonService;
+        private PermissionService? _permissionService;
+        private IConfiguration? _configuration;
+        private Guid _adminUserId;
+        private Guid _anotherUserId;
+        private Guid _moderatorUserId;
+
+        [SetUp]
+        public void Init()
+        {
+            var options = new DbContextOptionsBuilder<CarRacingTournamentDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new CarRacingTournamentDbContext(options);
+
+            _adminUserId = Guid.NewGuid();
+            Models.User adminUser = new Models.User
+            {
+                Id = _adminUserId,
+                Username = "TestUser",
+                Email = "test@test.com",
+                Password = "$2a$10$/Mw2QNUGYbV1AIyQ8QxXC.IhNRrmjwAW9SBgUv8Vh9xX2goWsQwG."
+            };
+
+            _moderatorUserId = Guid.NewGuid();
+            Models.User moderatorUser = new Models.User
+            {
+                Id = _moderatorUserId,
+                Username = "TestUser2",
+                Email = "test2@test.com",
+                Password = "$2a$10$/Mw2QNUGYbV1AIyQ8QxXC.IhNRrmjwAW9SBgUv8Vh9xX2goWsQwG."
+            };
+
+            _anotherUserId = Guid.NewGuid();
+            Models.User anotherUser = new Models.User
+            {
+                Id = _anotherUserId,
+                Username = "TestUser3",
+                Email = "test3@test.com",
+                Password = "$2a$10$/Mw2QNUGYbV1AIyQ8QxXC.IhNRrmjwAW9SBgUv8Vh9xX2goWsQwG."
+            };
+
+            Season season = new Season
+            {
+                Id = Guid.NewGuid(),
+                Name = "First Season",
+                Permissions = new List<Permission> {
+                    new Permission {
+                        Id = Guid.NewGuid(),
+                        User = adminUser,
+                        UserId = _adminUserId,
+                        Type = PermissionType.Admin
+                    },
+                    new Permission {
+                        Id = Guid.NewGuid(),
+                        User = moderatorUser,
+                        UserId = _moderatorUserId,
+                        Type = PermissionType.Moderator
+                    }
+                },
+            };
+
+            _context.Seasons.Add(season!);
+            _context.SaveChanges();
+
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperConfig());
+            });
+            var mapper = mockMapper.CreateMapper();
+
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _seasonService = new SeasonService(_context, mapper);
+            _permissionService = new PermissionService(_context);
+
+            _permissionController = new PermissionController(
+                _permissionService,
+                _seasonService
+            );
+        }
+
+        private void SetAuthentication(Guid? userId) {
+            var httpContextAccessor = new Mock<IHttpContextAccessor>();
+            var httpContext = new DefaultHttpContext();
+            if (userId != null) {
+                var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userId.ToString()!) });
+                var principal = new ClaimsPrincipal(identity);
+                httpContext.User = principal;
+            }
+            httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext);
+
+            _permissionController!.ControllerContext.HttpContext = httpContext;
+        }
+
+        [Test]
+        public async Task UpgradePermissionNotFound()
+        {
+            SetAuthentication(_anotherUserId);
+        }
+
+        [Test]
+        public async Task PutRaceAnotherUser() {
+            SetAuthentication(_anotherUserId);
+
+            var result = await _raceController!.Put(_race!.Id, new RaceDto {
+                Name = "NewRace",
+                DateTime = DateTime.Now
+            });
+
+            Assert.That(result, Is.TypeOf<ForbidResult>());
+        }
+
+        [Test]
+        public async Task PutRaceNotFound() {
+            SetAuthentication(_moderatorUserId);
+
+            var result = await _raceController!.Put(Guid.NewGuid(), new RaceDto {
+                Name = "NewRace",
+                DateTime = DateTime.Now
+            });
+
+            Assert.That(result, Is.TypeOf<NotFoundObjectResult>());
+        }
+
+        [Test]
+        public async Task PutRaceSeasonArchived() {
+            SetAuthentication(_moderatorUserId);
+
+            _race!.Season!.IsArchived = true;
+
+            var result = await _raceController!.Put(_race!.Id, new RaceDto {
+                Name = "NewRace",
+                DateTime = DateTime.Now
+            });
+
+            Assert.That(result, Is.TypeOf<BadRequestObjectResult>());
+        }
+
+        [Test]
+        public async Task DeleteRaceAnotherUser() {
+            SetAuthentication(_anotherUserId);
+
+            var result = await _raceController!.Delete(_race!.Id);
+
+            Assert.That(result, Is.TypeOf<ForbidResult>());
+        }
+
+        [Test]
+        public async Task DeleteRaceNotFound() {
+            SetAuthentication(_moderatorUserId);
+
+            var result = await _raceController!.Delete(Guid.NewGuid());
+
+            Assert.That(result, Is.TypeOf<NotFoundObjectResult>());
+        }
+
+        [Test]
+        public async Task DeleteRaceArchived() {
+            SetAuthentication(_moderatorUserId);
+
+            _race!.Season!.IsArchived = true;
+
+            var result = await _raceController!.Delete(_race!.Id);
+
+            Assert.That(result, Is.TypeOf<BadRequestObjectResult>());
+        }
+    }
+}
