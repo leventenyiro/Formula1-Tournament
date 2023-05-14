1895dab9f31dd38aee1fa2d6ae0835dc81991a13
leventenyiro <nyiro.levente@gmail.com>
2023-05-14 09:48:54 +0200

    #278: modify dev-prod

--- car-racing-tournament-api/Program.cs
+++ car-racing-tournament-api/Program.cs
@@ -20,7 +20,7 @@ internal class Program
         builder.Services.AddControllers();
         builder.Services.AddDbContext<CarRacingTournamentDbContext>(options =>
         {
-            if (bool.Parse(builder.Configuration.GetSection("Development").Value)) {
+            if (builder.Environment.IsDevelopment()) {
                 options.UseMySql("Server=localhost;Uid=root;Database=car_racing_tournament;Pwd=;", 
                     new MySqlServerVersion(new Version(8, 0, 22)), 
                     options => {
